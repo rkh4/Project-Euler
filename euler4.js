622d0a204dcd1ceca51d4753da2de380c62b0adf
rhys.howard <rhys.howard@egress.com>
2016-05-18 17:44:25 +0100

    completed euler's 4th task, confirmed correct answer

---
+++ euler4.js
@@ -0,0 +1,30 @@
+ palindromic number reads the same both ways. The largest palindrome made from the 
+*      product of two 2-digit numbers is 9009 = 91 × 99.
+*  Find the largest palindrome made from the product of two 3-digit numbers.
+*/
+
+function euler4(){
+	//Initialisation
+  var product1 = 999;
+  var product2 = 999;
+  var result;
+  var largestResult = 0;
+  //Loop through products
+  for (var i = 1; i <= product1; i++){
+  	for (var j = 1; j <= product2; j++){
+    	result = i * j;
+      reverseResult = String(result).split("").reverse().join("");
+      if (String(result) == reverseResult){
+      	console.log(i +" * "+ j +" = "+ result);
+      	console.log("reverse = " + reverseResult);
+        console.log("Palindrome!");
+        if (result > largestResult){
+        	largestResult = result;
+       	}
+      }
+    }
+  }
+  console.log("Largest result for any two 3 digit numbers, is: " + largestResult);
+}
+
+euler4();
